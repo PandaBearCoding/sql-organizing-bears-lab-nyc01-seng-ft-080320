723c5f14f150219f3aea7670d66fccb8f57bd1b3
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-08-12 17:58:04 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,13 @@
 CREATE TABLE bears (
   //columns here
 );
+
+CREATE TABLE cats (
+  id INTEGER PRIMARY KEY,
+  name TEXT, 
+  age INTEGER
+  gender TEXT
+  color TEXT
+  temperament TEXT
+  alive BOOLEAN
+);
