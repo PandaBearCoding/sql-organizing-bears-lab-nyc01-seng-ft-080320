318823518b047da8cb941631a351cfae5aa113ff
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-08-12 18:01:02 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,3 +7,14 @@ CREATE TABLE bears (
   temperament TEXT,
   alive INTEGER
 );
+
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
